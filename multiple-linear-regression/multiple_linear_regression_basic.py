5969c0e1386cd99fd0f0f6824a1eaee5f13c6a10
SMART2016 <javahub@yahoo.com>
2019-05-18 11:33:59 +0530

    Added basic implementation of Multiple linear regression

--- multiple-linear-regression/multiple_linear_regression_basic.py
+++ multiple-linear-regression/multiple_linear_regression_basic.py
@@ -40,31 +40,21 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
-#Building the optimal model using backward elimination
-# the library for creating model do not handle the constant b0 so need to add to the dataset.
+# Building the optimal model using Backward Elimination
 import statsmodels.formula.api as sm
-#np.append([[1,2],[3,4]],[[1],[10]], axis=1)
-#O/p: array([[ 1,  2,  1],
-#       [ 3,  4, 10]])
-#for handling constant b0 adding x0 = 1 to the X data set
-X = np.append(arr = np.ones((50,1)).astype(int),values= X,axis = 1)
-# Step 1: 
-X_OPT = X[:,[0,1,2,3,4,5]]
-# Step 3: calculating the stats for the sample
-regressor_OLS = sm.OLS(endog=y ,exog= X_OPT).fit()
-
-#Step 4: Iterative step of removing predictors or independent variables one by one based on there P-Value > 0.05,
-#until the remaining variables P-Value is less than 0.05
-# also if the adjusted Rsquare before removal of the variable is greater than the adj-R^2 after removal that means ,
-# we should not remove the variable from the predictor set.
-pVals = regressor_OLS.pvalues
-sigLevel = 0.05
-while pVals[np.argmax(pVals)] > sigLevel:
-     X_OPT = np.delete(X_OPT, np.argmax(pVals), axis = 1)
-     print("pval of dim removed: " + str(np.argmax(pVals)))
-     print(str(X_OPT.shape[1]) + " dimensions remaining...")
-     regressor_OLS = sm.OLS(endog = y, exog = X_OPT).fit()
-     pVals = regressor_OLS.pvalues
-
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
